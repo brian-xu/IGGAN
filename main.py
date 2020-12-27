3973353259fd4814b68af5326b1718fcd853f4cf
Brian Xu <xu.brian.s@gmail.com>
2020-12-26 18:41:19 -0800

    Progress but needs a lot of tweaking

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import argparse
-import model
 import random
+import numpy as np
 import torch
 import torch.optim as optim
 import torch.nn as nn
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
-import torchvision.utils as vutils
+import cv2
+import model
 
 # Set random seed for reproducibility
 import render
@@ -16,10 +17,10 @@ random.seed(manualSeed)
 torch.manual_seed(manualSeed)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default="data/500")
-parser.add_argument('--batch_size', type=int, default=64)
+parser.add_argument('--dataset', type=str, default="data/")
+parser.add_argument('--batch_size', type=int, default=5)
 parser.add_argument('--num_epochs', type=int, default=5)
-parser.add_argument('--workers', type=int, default=2)
+parser.add_argument('--workers', type=int, default=1)
 parser.add_argument('--nr_lr', type=float, default=2e-5)
 parser.add_argument('--gan_lr', type=float, default=2e-4)
 parser.add_argument('--beta1', type=float, default=0.5)
@@ -33,8 +34,14 @@ args = parser.parse_args()
 
 device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
 
+if args.is_grayscale:
+    img_transform = transforms.Compose([transforms.Grayscale(num_output_channels=1),
+                                        transforms.ToTensor()])
+else:
+    img_transform = transforms.ToTensor()
+
 dataset = dset.ImageFolder(root=args.dataset,
-                           transform=transforms.ToTensor())
+                           transform=img_transform)
 # Create the dataloader
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size,
                                          shuffle=True, num_workers=args.workers)
@@ -59,11 +66,11 @@ renderer.apply(weights_init)
 
 criterion = nn.BCELoss()
 
-l2 = nn.MSELoss(reduction='sum')
+l2 = nn.MSELoss()
 
 
 def DOMLoss(x, y):
-    return torch.square(torch.log(x) - torch.log(y))
+    return torch.mean(torch.square(torch.log(x) - torch.log(y)))
 
 
 fixed_noise = torch.randn(64, args.z_size, 1, 1, device=device)
@@ -75,110 +82,124 @@ optimizerG = optim.Adam(generator.parameters(), lr=args.gan_lr, betas=(args.beta
 optimizerD = optim.Adam(discriminator.parameters(), lr=args.gan_lr, betas=(args.beta1, 0.999))
 optimizerR = optim.Adam(renderer.parameters(), lr=args.nr_lr, betas=(args.beta1, 0.999))
 
-img_list = []
 G_losses = []
 D_losses = []
 R_losses = []
-iters = 0
 
-print("Starting Training Loop...")
-# For each epoch
-for epoch in range(args.num_epochs):
-    # For each batch in the dataloader
-    for i, data in enumerate(dataloader):
+# fake_voxels = torch.zeros(1,1,64,64,64)
+#
+# for i in range(64):
+#     for j in range(64):
+#         for k in range(64):
+#             fake_voxels[0,0,i, j, k] = 1
 
-        ############################
-        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
-        ###########################
-        # Train with all-real batch
-        discriminator.zero_grad()
-        # Format batch
-        real_cpu = data[0].to(device)
-        b_size = real_cpu.size(0)
-        label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
-        # Forward pass real batch through D
-        output = discriminator(real_cpu).view(-1)
-        # Calculate loss on all-real batch
-        errD_real = criterion(output, label)
-        # Calculate gradients for D in backward pass
-        errD_real.backward()
-        D_x = output.mean().item()
 
-        # Train with all-fake batch
-        # Generate batch of latent vectors
-        noise = torch.randn(b_size, args.z_size, 1, 1, device=device)
-        # Generate fake voxels batch with G
-        fake_voxels = generator(noise)
-        # Render fake voxel batch with R
-        fake = renderer(fake_voxels)
-        label.fill_(fake_label)
-        # Classify all fake batch with D
-        output = discriminator(fake.detach()).view(-1)
-        # Calculate D's loss on the all-fake batch
-        errD_fake = criterion(output, label)
-        # Calculate the gradients for this batch
-        errD_fake.backward()
-        D_G_z1 = output.mean().item()
-        # Add the gradients from the all-real and all-fake batches
-        errD = errD_real + errD_fake
-        # Update D
-        optimizerD.step()
 
-        ############################
-        # (2) Update G network: maximize log(D(G(z)))
-        ###########################
-        generator.zero_grad()
-        label.fill_(real_label)  # fake labels are real for generator cost
-        # Since we just updated D, perform another forward pass of all-fake batch through D
-        output = discriminator(fake).view(-1)
-        # Calculate G's loss based on this output
-        errG = criterion(output, label)
-        # Calculate gradients for G
-        errG.backward()
-        D_G_z2 = output.mean().item()
-        # Update G
-        optimizerG.step()
+def main():
+    print("Starting Training Loop...")
+    # For each epoch
+    for epoch in range(args.num_epochs):
+        # For each batch in the dataloader
+        for i, data in enumerate(dataloader):
+            ############################
+            # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+            ###########################
+            # Train with all-real batch
+            discriminator.zero_grad()
+            # Format batch
+            real_cpu = data[0].to(device)
+            b_size = real_cpu.size(0)
+            label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
+            # Forward pass real batch through D
+            output = discriminator(real_cpu).view(-1)
+            # Calculate loss on all-real batch
+            errD_real = criterion(output, label)
+            # Calculate gradients for D in backward pass
+            errD_real.backward()
+            D_x = output.mean().item()
+            del real_cpu
 
-        ############################
-        # (3) Update R network: minimize l2(R) + lambda * DOM(R)
-        ###########################
-        renderer.zero_grad()
-        # Since we just updated G, generate a new fake batch
-        fake_voxels = generator(noise)
-        # Render the voxels with the neural renderer
-        nr = renderer(fake_voxels)
-        # Render the voxels with an off-the-shelf renderer
-        ots = render.render_canonical(fake_voxels)
-        # Perform a forward pass of neural renderer output through D
-        nr_output = discriminator(nr).view(-1)
-        # Perform a forward pass of off-the-shelf renderer output through D
-        ots_output = discriminator(ots).view(-1)
-        # Calculate R's L2 loss based on squared error of pixel matrix
-        errL2 = l2(fake, ots)
-        # Calculate R's DOM loss based on squared log error of discriminator output
-        errDOM = DOMLoss(ots_output, nr_output)
-        errR = errL2 + args.dom_lambda * errDOM
-        # Calculate gradients for R
-        errR.backward()
-        R_x = output.mean().item()
-        # Update R
-        optimizerR.step()
+            # Train with all-fake batch
+            # Generate batch of latent vectors
+            noise = torch.randn(b_size, args.z_size, 1, 1, device=device)
+            # Generate fake voxels batch with G
+            fake_voxels = generator(noise)
+            # Render fake voxel batch with R
+            fake = renderer(fake_voxels)
+            del fake_voxels
+            label.fill_(fake_label)
+            # Classify all fake batch with D
+            output = discriminator(fake.detach()).view(-1)
+            # Calculate D's loss on the all-fake batch
+            errD_fake = criterion(output, label)
+            # Calculate the gradients for this batch
+            errD_fake.backward()
+            D_G_z1 = output.mean().item()
+            # Add the gradients from the all-real and all-fake batches
+            errD = errD_real + errD_fake
+            # Update D
+            optimizerD.step()
 
-        # Output training stats
-        if i % 50 == 0:
-            print(f"[{epoch}/{args.num_epochs}][{i}/{len(dataloader)}]\t"
-                  f"Loss_D: {errD.item():.4f}\tLoss_G: {errG.item():.4f}\tLoss_R: {errR.item():.4f}\t"
-                  f"D(x):  {D_x.item():.4f}\tD(G(z)):  {D_G_z1:.4f}/ {D_G_z2:.4f}\tR(G(z):  {R_x:.4f}")
+            ############################
+            # (2) Update G network: maximize log(D(G(z)))
+            ###########################
+            generator.zero_grad()
+            label.fill_(real_label)  # fake labels are real for generator cost
+            # Since we just updated D, perform another forward pass of all-fake batch through D
+            output = discriminator(fake).view(-1)
+            # Calculate G's loss based on this output
+            errG = criterion(output, label)
+            # Calculate gradients for G
+            errG.backward()
+            D_G_z2 = output.mean().item()
+            # Update G
+            optimizerG.step()
 
-        # Save Losses for plotting later
-        G_losses.append(errG.item())
-        D_losses.append(errD.item())
-        R_losses.append(errR.item())
+            ############################
+            # (3) Update R network: minimize l2(R) + lambda * DOM(R)
+            ###########################
+            renderer.zero_grad()
+            # Since we just updated G, generate a new fake batch
+            fake_voxels = generator(noise)
+            # Render the voxels with the neural renderer
+            nr = renderer(fake_voxels)
+            # Render the voxels with an off-the-shelf renderer
+            ots_results = []
+            for ex in range(fake_voxels.shape[0]):
+                img = np.expand_dims(render.render_canonical(fake_voxels[ex, 0], True), axis=(0, 1))
+                ots_results.append(img)
+            ots = torch.tensor(np.vstack(ots_results), dtype=torch.float).to(device) / 255
+            cv2.imwrite(f'testing/nr_{epoch}_{i}.jpg', nr.detach().cpu()[0, 0].numpy() * 255)
+            cv2.imwrite(f'testing/ots_{epoch}_{i}.jpg', ots.detach().cpu()[0, 0].numpy() * 255)
+            # Perform a forward pass of neural renderer output through D
+            nr_output = discriminator(nr).view(-1)
+            # Perform a forward pass of off-the-shelf renderer output through D
+            ots_output = discriminator(ots).view(-1)
+            # Calculate R's L2 loss based on squared error of pixel matrix
+            errL2 = l2(nr, ots)
+            # Calculate R's DOM loss based on squared log error of discriminator output
+            errDOM = DOMLoss(ots_output, nr_output)
+            errR = errL2 + args.dom_lambda * errDOM
+            # Calculate gradients for R
+            errR.backward()
+            R_x = nr_output.mean().item()
+            # Update R
+            optimizerR.step()
 
-        # Check how the generator is doing by saving G's output on fixed_noise
-        if (iters % 500 == 0) or ((epoch == args.num_epochs - 1) and (i == len(dataloader) - 1)):
-            with torch.no_grad():
-                fake = generator(fixed_noise).detach().cpu()
-            img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+            # Output training stats
+            if i % 50 == 0:
+                print(f"[{epoch}/{args.num_epochs}][{i}/{len(dataloader)}]\t"
+                      f"Loss_D: {errD.item():.4f}\tLoss_G: {errG.item():.4f}\tLoss_R: {errR.item():.4f}\t"
+                      f"D(x):  {D_x:.4f}\tD(G(z)):  {D_G_z1:.4f}/ {D_G_z2:.4f}\tR(G(z):  {R_x:.4f}")
 
-        iters += 1
+            # Save Losses for plotting later
+            G_losses.append(errG.item())
+            D_losses.append(errD.item())
+            R_losses.append(errR.item())
+
+            # with torch.no_grad():
+            #     fake = generator(fixed_noise).detach().cpu()
+
+
+if __name__ == '__main__':
+    main()
--- render.py
+++ render.py
@@ -1,6 +1,7 @@
 import pyrender
 import numpy as np
 import trimesh
+import cv2
 
 
 def mesh_colors():
@@ -10,30 +11,31 @@ def mesh_colors():
     return [r, g, r, b, b, g, b, g, b, g, r, r]
 
 
-def render_canonical(fake_voxels):
-    coords = fake_voxels[0, 0]
-    points = (coords > 0.5).nonzero(as_tuple=False).cpu()
+sm = trimesh.creation.box((1, 1, 1))
+sm.visual.face_colors = mesh_colors()
 
-    sm = trimesh.creation.box((1, 1, 1))
-    sm.visual.face_colors = mesh_colors()
+camera = pyrender.OrthographicCamera(xmag=80, ymag=80, zfar=1000)
+camera_pose = np.array([
+    [-6.43043726e-01, 3.82967760e-01, -6.63165959e-01, -2.27456165e+01],
+    [2.54106097e-04, 8.65931585e-01, 4.99881402e-01, 7.32862870e+01],
+    [7.65963251e-01, 3.21222953e-01, -5.56908875e-01, -1.39929237e+01],
+    [0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 1.00000000e+00]
+])
+scene = pyrender.Scene(bg_color=np.ones(4), ambient_light=np.ones(3))
+camera_node = scene.add(camera, pose=camera_pose)
+
+
+def render_canonical(fake_voxels, is_grayscale, threshold=0.5):
+    points = (fake_voxels > threshold).nonzero(as_tuple=False).cpu()
+
     tfs = np.tile(np.eye(4), (len(points), 1, 1))
     tfs[:, :3, 3] = points
-    m = pyrender.Mesh.from_trimesh(sm, poses=tfs, smooth=False)
-
-    camera = pyrender.OrthographicCamera(xmag=80, ymag=80, zfar=1000)
-    camera_pose = np.array([
-        [-6.43043726e-01, 3.82967760e-01, -6.63165959e-01, -2.27456165e+01],
-        [2.54106097e-04, 8.65931585e-01, 4.99881402e-01, 7.32862870e+01],
-        [7.65963251e-01, 3.21222953e-01, -5.56908875e-01, -1.39929237e+01],
-        [0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 1.00000000e+00]
-    ])
-    scene = pyrender.Scene(bg_color=np.ones(4), ambient_light=np.ones(3))
-
-    scene.add(m)
-
-    camera_node = scene.add(camera, pose=camera_pose)
+    mesh = pyrender.Mesh.from_trimesh(sm, poses=tfs, smooth=False)
+    mesh_node = scene.add(mesh)
 
     r = pyrender.OffscreenRenderer(viewport_width=256, viewport_height=256, point_size=1)
-
     color, depth = r.render(scene)
-    return color
+    r.delete()
+    scene.remove_node(mesh_node)
+
+    return cv2.cvtColor(color, cv2.COLOR_BGR2GRAY) if is_grayscale else color
